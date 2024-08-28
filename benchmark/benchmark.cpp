89a7989c6c0a8f50bf1c7e131b83502c896f4c19
Andrei Ilin <ortfero@gmail.com>
2024-08-28 13:18:32 +0300

    Use fixed buffer for ufmt

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -13,20 +13,25 @@
 int main() {
     using namespace std;
 
-    ufmt::text text;
+    ufmt::fixed_text text;
     char charz[128];
 
     auto const texter_char = ubench::run([&] {
         text.clear();
-        text.format('a');
+        auto c = 'x';
+        text.format(c);
     });
-    auto const snprintf_char =
-        ubench::run([&] { snprintf(charz, sizeof(charz), "%c", 'a'); });
-    auto const fmt_char =
-        ubench::run([&] { fmt::format_to(charz, "{}", 'a'); });
+    auto const snprintf_char = ubench::run([&] {
+        auto c = 'x';
+        snprintf(charz, sizeof(charz), "%c", c);
+    });
+    auto const fmt_char = ubench::run([&] {
+        auto c = 'x';
+        fmt::format_to(charz, "{}", c);
+    });
 
-    cout << "texter.print(char) - " << texter_char << '\n';
-    cout << "snprintf(char)     - " << snprintf_char << '\n';
+    cout << "texter.print(char)    - " << texter_char << '\n';
+    cout << "snprintf(char)        - " << snprintf_char << '\n';
     cout << "fmt::format_to(char)  - " << fmt_char << '\n';
     cout << endl;
 
@@ -34,31 +39,36 @@ int main() {
         text.clear();
         text.format("some literal");
     });
-    auto const snprintf_literal = ubench::run(
-        [&] { snprintf(charz, sizeof(charz), "%s", "some literal"); });
-    auto const fmt_literal =
-        ubench::run([&] { fmt::format_to(charz, "{}", "some literal"); });
+    auto const snprintf_literal = ubench::run([&] {
+        snprintf(charz, sizeof(charz), "%s", "some literal");
+    });
+    auto const fmt_literal = ubench::run([&] {
+        fmt::format_to(charz, "{}", "some literal");
+    });
 
-    cout << "texter.print(char[N]) - " << texter_literal << '\n';
-    cout << "snprintf(char[N])     - " << snprintf_literal << '\n';
+    cout << "texter.print(char[N])    - " << texter_literal << '\n';
+    cout << "snprintf(char[N])        - " << snprintf_literal << '\n';
     cout << "fmt::format_to(char[N])  - " << fmt_literal << '\n';
     cout << endl;
 
     auto const texter_int = ubench::run([&] {
         text.clear();
-        text.format(-127562);
+        text.format(rand());
     });
-    auto const snprintf_int =
-        ubench::run([&] { snprintf(charz, sizeof(charz), "%d", -127562); });
-    auto const fmt_int =
-        ubench::run([&] { fmt::format_to(charz, "{}", -127562); });
-    auto const tochars_int = ubench::run(
-        [&] { std::to_chars(charz, charz + sizeof(charz), -127562); });
+    auto const snprintf_int = ubench::run([&] {
+        snprintf(charz, sizeof(charz), "%d", rand());
+    });
+    auto const fmt_int = ubench::run([&] {
+        fmt::format_to(charz, "{}", rand());
+    });
+    auto const tochars_int = ubench::run([&] {
+        std::to_chars(charz, charz + sizeof(charz), rand());
+    });
 
-    cout << "texter.print(int)  - " << texter_int << '\n';
-    cout << "snprintf(int)      - " << snprintf_int << '\n';
+    cout << "texter.print(int)     - " << texter_int << '\n';
+    cout << "snprintf(int)         - " << snprintf_int << '\n';
     cout << "fmt::format_to(int)   - " << fmt_int << '\n';
-    cout << "std::to_chars(int) - " << tochars_int << '\n';
+    cout << "std::to_chars(int)    - " << tochars_int << '\n';
     cout << endl;
 
     auto const texter_double = ubench::run([&] {
@@ -71,8 +81,8 @@ int main() {
         ubench::run([&] { fmt::format_to(charz, "{}", -127562.127562); });
 
 
-    cout << "texter.print(double)  - " << texter_double << '\n';
-    cout << "snprintf(double)      - " << snprintf_double << '\n';
+    cout << "texter.print(double)     - " << texter_double << '\n';
+    cout << "snprintf(double)         - " << snprintf_double << '\n';
     cout << "fmt::format_to(double)   - " << fmt_double << '\n';
     cout << endl;
 
@@ -86,11 +96,11 @@ int main() {
     auto const fmt_format = ubench::run(
         [&] { fmt::format_to(charz, "nums: {}, {}, {}", -1, -2, -3); });
 
-    cout << "texter.print(\"nums: \", -1, \", \", -2, \", \", -3)  - "
+    cout << "texter.print('nums: ', -1, ', ', -2, ', ', -3)    - "
          << texter_format << '\n';
-    cout << "snprintf(\"nums: %d, %d, %d\", -1, -2, -3)      - "
+    cout << "snprintf('nums: %d, %d, %d', -1, -2, -3)          - "
          << snprintf_format << '\n';
-    cout << "fmt::format_to(\"nums: {}, {}, {}\", -1, -2, -3)   - "
+    cout << "fmt::format_to('nums: {}, {}, {}', -1, -2, -3)    - "
          << fmt_format << '\n';
     cout << endl;
 
